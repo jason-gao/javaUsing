7236e0ffe4ad0d6c4af983d3d0c94433b2ea0e35
jason-gao <3048789891@qq.com>
2019-05-20 13:47:41 +0800

    update

---
+++ README.md
@@ -0,0 +1,6 @@
+# java-sdk install
+
+- yum search jdk
+- yum intall java-1.8.0-openjdk.x86_64
+- java -version
+
---
+++ src/main/java/com/xyui/es/Demo.java
@@ -0,0 +1,10 @@
+package com.xyui.es;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Demo {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello World!");
+    }
+}
